dc63d702af1019206b9bc904be7d706ab3d1e0a6
Dushess0 <ilyabannila@gmail.com>
2019-11-18 00:34:30 +0100

    Lidar is working

--- simulationBackend/Assets/Scripts/LidarSensor.cs
+++ simulationBackend/Assets/Scripts/LidarSensor.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
+
+
 public class LidarSensor : MonoBehaviour
 {
     
@@ -19,38 +22,95 @@ public class LidarSensor : MonoBehaviour
     public float RotationSpeed; //Hz
     [Tooltip("Starting position of lasers")]
     public Transform StartPosition;
-    
-    
- 
+
+    Ray[] LaserRays;
+    float[] LaserAngles;
+    public List<float[]> Data;
+    [Tooltip("Enable rays and hits in editor but disable collecting data !!!! ")]
+    public bool Debugging;
+    public float  Magnitude;
+
     void FixedUpdate()
     {
         
-        this.transform.Rotate(0, Time.fixedDeltaTime * RotationSpeed*360, 0);
+        this.transform.Rotate(0, Time.fixedDeltaTime * RotationSpeed*360, 0,Space.Self);
+        UpdateRays();
+        DetectCollisions();
 
 
     }
-    void OnDrawGizmosSelected()
-    {
 
-        Gizmos.color = Color.red;
-        float magnitude = 3;
+    void Awake()
+    {
+        Data = new List<float[]>();      
+        LaserRays = new Ray[LasersCount];
+        LaserAngles = new float[LasersCount];
         for (int i = 0; i < LasersCount; i++)
         {
-            
-            Vector3 LaserPosition = StartPosition.position + new Vector3(0, -LaserInterval*i/100, 0);
-            float currentangle= (Degree + i * DegreeDelta)*Mathf.Deg2Rad;
+            LaserAngles[i] = Degree + i * DegreeDelta;
+            LaserRays[i]=new Ray();
 
-            
-
-            
-            Vector3 PointingAt = LaserPosition + new Vector3(0, -Mathf.Sin(currentangle) * magnitude, Mathf.Cos(currentangle) * magnitude);
-            
-
-            Gizmos.DrawWireSphere(LaserPosition, 0.01f);
-            Gizmos.DrawLine(LaserPosition,  PointingAt);
-           
         }
 
+
+    }
+    void UpdateRays()
+    {
+
+        for (int i = 0; i < LasersCount; i++)
+        {
+
+            LaserRays[i].origin = StartPosition.position + new Vector3(0, -LaserInterval * i / 100, 0);
+            LaserRays[i].direction= Vector3.up;
+
+            LaserRays[i].direction = Quaternion.AngleAxis(-LaserAngles[i] - 90, Vector3.right) * LaserRays[i].direction;
+            LaserRays[i].direction = Quaternion.AngleAxis(this.transform.rotation.eulerAngles.y, Vector3.up) * LaserRays[i].direction;
+
+
+        }
+        
+       
+    }
+    void DetectCollisions()
+    {
+        float[] row = new float[LasersCount];
+        for (int i = 0; i < LasersCount; i++)
+        {
+            RaycastHit hit;
+            Debug.DrawLine(LaserRays[i].origin,LaserRays[i].origin+ LaserRays[i].direction * Magnitude, Color.red); 
+            if (Physics.Raycast(LaserRays[i],out hit,Magnitude)) //4 is layer mask, 4 is used for water
+            {
+                row[i] = hit.distance;
+                
+                Debug.Log("hit");
+                Debug.DrawLine(LaserRays[i].origin, LaserRays[i].origin + LaserRays[i].direction * Magnitude, Color.green);
+
+            }
+                
+        }
+        Data.Add(row);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (Debugging)
+        {
+            Awake();
+            UpdateRays();
+            DetectCollisions();
+            Gizmos.color = Color.red;
+
+
+            for (int i = 0; i < LasersCount; i++)
+            {
+
+                
+                Gizmos.DrawWireSphere(LaserRays[i].origin, 0.01f);
+                Gizmos.DrawLine(LaserRays[i].origin, LaserRays[i].origin + LaserRays[i].direction * Magnitude);
+
+
+            }
+        }
     }
 
     
