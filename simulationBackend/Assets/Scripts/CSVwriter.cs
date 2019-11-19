43326cba647b2d365e1e539b7e9ffe54b53527f1
Bohdan Forostianyi <qwebeck5@gmail.com>
2019-11-19 18:02:44 +0100

    Made writer for lidar data

--- simulationBackend/Assets/Scripts/CSVwriter.cs
+++ simulationBackend/Assets/Scripts/CSVwriter.cs
@@ -8,18 +8,26 @@ namespace CSVWriter{
 	
     public class DataRow
     {
+        private static int rowsInDataset = 0;
+        // fix 
+        public int rowNumber; 
         private byte[] firstCamera ;
         private byte[] secondCamera;
         private byte[] thirdCamera ;
         private List<Vector3> lidarData;
         private List<Vector2> ultrasoundData;
+
+        public string lidarDataLoaction;
         // two floats indicating state of each engine
         private float leftEngine;  
         private float rightEngine;
+
+        
         
 		public static void Main(){
 			 Console.WriteLine("DataRow");
 		}
+
 		
         public byte[] FirstCamera
 		{
@@ -36,16 +44,7 @@ namespace CSVWriter{
             get { return this.thirdCamera; }
             set { this.thirdCamera = value; }
         }
-        public List<Vector3> LidarData
-        {
-            get { return this.lidarData; }
-            set { this.lidarData = value; }
-        }
-        public List<Vector2> UltrasoundData
-        {
-            get { return this.ultrasoundData; }
-            set { this.ultrasoundData = value; }
-        }
+        
         public float LeftEngine
         {
             get { return this.leftEngine; }
@@ -56,9 +55,19 @@ namespace CSVWriter{
             get { return this.rightEngine; }
             set { this.RightEngine= value; }
         }
+        
+        public void writeAdditionalData(){
+				using (var writer = new StreamWriter(lidarDataLoaction))
+    			using (var csv = new CsvWriter(writer))
+    			{
+                    csv.WriteRecords(this.lidarData);                    
+    			}
+        }
 
         internal DataRow(byte[] fC, byte[] sC, byte[] tC,List<Vector3> lData, List<Vector2> uData,float[] engine_state )
         {
+            DataRow.rowsInDataset += 1;
+            this.rowNumber = rowsInDataset; 
             this.firstCamera = fC;
             this.secondCamera = sC;
             this.thirdCamera = tC;
@@ -71,19 +80,36 @@ namespace CSVWriter{
 
     public class Writer
     {
-        private string csvFileName;
+        private string lidarDirectory = "lidar_data";
+        private string csvFileName = "data_set.csv";
+
         private List<DataRow> records = new List<DataRow>();
-		
+		internal Writer(){
+            if (Directory.Exists(lidarDirectory)) 
+            {
+                Console.WriteLine("That path exists already.");
+            }
+            else
+            {
+                Directory.CreateDirectory(lidarDirectory);
+                Console.WriteLine("The directory was created successfully ");
+            }
+        }
 			
         public void write()
         {
-				  using (var writer = new StreamWriter("data_set.csv"))
-    			  using (var csv = new CsvWriter(writer))
-    			  {
-                
-                    csv.WriteRecords(records);
-        			
-    			  }
+            // fill location of lidar data
+                foreach (DataRow row in records)
+                {
+                    row.lidarDataLoaction = lidarDirectory + "/lidar_data_" + row.rowNumber.ToString() + ".csv";
+                    row.writeAdditionalData();
+                }                
+				using (var writer = new StreamWriter(csvFileName))
+    			using (var csv = new CsvWriter(writer))
+    			{
+                    csv.WriteRecords(records);                    
+    			}
+
             Debug.Log("Succesfully saved training data");
 
         }
