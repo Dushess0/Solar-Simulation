26ac4c84c5274e07b28555c1bf9b43130409d148
Dushess0 <ilyabannila@gmail.com>
2019-11-25 23:17:27 +0100

    Update client.py

--- pythonWrapper/client.py
+++ pythonWrapper/client.py
@@ -4,7 +4,8 @@ train_mode = False
 #env = UnityEnvironment(file_name="compiled-LINUX/BoatSim") # for linux
 
 #env = UnityEnvironment(file_name="compiled/BoatSimulation") # for windows
-env = UnityEnvironment(file_name="compiled-OSX.app")
+env = UnityEnvironment(file_name="compiled-OSX.app") # for OS X
+
 # [left,right] left - left and right engines signal (1 means forward, 0 do nothing,-1 backward)
 env.reset(train_mode=train_mode, config=None)
 for i in range(1,200): 
