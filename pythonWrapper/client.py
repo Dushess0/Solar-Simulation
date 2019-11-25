2c6afe6f0c176a3521f4dbe8d2a9c74b81d5e211
Dushess0 <ilyabannila@gmail.com>
2019-11-25 23:17:19 +0100

    added OS X and started ros integration

--- pythonWrapper/client.py
+++ pythonWrapper/client.py
@@ -1,12 +1,15 @@
 from mlagents.envs.environment import UnityEnvironment
 
 train_mode = False
+#env = UnityEnvironment(file_name="compiled-LINUX/BoatSim") # for linux
 
-env = UnityEnvironment(file_name="compiled-LINUX/BoatSim")
+#env = UnityEnvironment(file_name="compiled/BoatSimulation") # for windows
+env = UnityEnvironment(file_name="compiled-OSX.app")
 # [left,right] left - left and right engines signal (1 means forward, 0 do nothing,-1 backward)
 env.reset(train_mode=train_mode, config=None)
-env.step([1,1], memory=None, text_action=None) # move forward for one step
-
+for i in range(1,200): 
+  env.step([1,1], memory=None, text_action=None) # move forward for one step
+  
 
 
 env.close() # close enviroment
